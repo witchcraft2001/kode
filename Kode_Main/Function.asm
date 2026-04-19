5cca7bb65ca47100e2c168d586f1bc9faf8e8ffa
DmitryM <mikhaltchenkov@gmail.com>
2026-04-19 10:15:28 +0500

    added backup file

--- Kode_Main/Function.asm
+++ Kode_Main/Function.asm
@@ -346,7 +346,9 @@ SaveFl1	INC	HL
 	OR	A
 	JR	NZ,SaveFl1
 	LD	(DE),A
-SaveF11	LD	A,C
+SaveF11	XOR	A
+	LD	(DE),A			; force zero-terminated FileName
+	LD	A,C
 	OUT	(SLOT2),A
 	LD	HL,FileName
 	LD	DE,Noname		
@@ -385,6 +387,7 @@ SaveFl3	LD	A,(IX+#1D)	; 1 text page
 	EX	AF,AF'
 	JP	C,SaveFileAs	
 SaveTAS:
+	CALL	MakeBackupIfExists
 	JP	ExportTXT
 	LD	(IY+#17),#01	; Was not touched
 	CALL	OnlySyntax
@@ -452,6 +455,311 @@ SaveFle	EX	AF,AF'
 	LD	HL,what
 	LD	(HL),evNothing
 	RET 
+
+;------------------------------------------------------------
+; Create backup by rename before save.
+; 1) Delete old backup if exists.
+; 2) Rename source file -> backup name.
+; Errors are ignored, main save must continue.
+MakeBackupIfExists:
+	PUSH	IY
+	IN	A,(SLOT0)
+	PUSH	AF
+	LD	A,(DOSpage)
+	OUT	(SLOT0),A
+	LD	BC,#7FFD
+	LD	A,#10
+	OUT	(C),A
+
+	CALL	BuildSourceName
+	CALL	BuildBackupName
+	JR	C,BkpDone
+
+	XOR	A
+	LD	HL,BackupName
+	LD	C,#0E			; DOS: Delete
+	RST	#10			; ignore errors (not exists)
+
+	XOR	A
+	LD	HL,BkpSrcName
+	LD	DE,BackupName
+	LD	C,#10			; DOS: Rename (old->new)
+	RST	#10
+	JR	NC,BkpDone
+
+	CALL	BkpCopyFallback
+
+BkpDone:
+	LD	BC,#7FFD
+	XOR	A
+	OUT	(C),A
+	POP	AF
+	OUT	(SLOT0),A
+	POP	IY
+	RET
+
+; Fallback if rename is unsupported/failed: copy source -> backup.
+; Expects DOS page and VG93 ports already enabled.
+BkpCopyFallback:
+	LD	A,#FF
+	LD	(BkpSrcH),A
+	LD	(BkpDstH),A
+	LD	HL,BkpSrcName
+	LD	A,#01
+	LD	C,#11			; DOS: Open existing (read)
+	RST	#10
+	JR	C,BkpCF_Fail
+	LD	(BkpSrcH),A
+
+	LD	HL,BackupName
+	XOR	A
+	LD	C,#0A			; DOS: Create backup
+	RST	#10
+	JR	C,BkpCF_FailCloseSrc
+	LD	(BkpDstH),A
+
+BkpCF_Loop:
+	LD	HL,FuncBuffer
+	LD	DE,#0100
+	LD	A,(BkpSrcH)
+	LD	C,#13			; DOS: Read
+	RST	#10
+	JR	C,BkpCF_FailCloseAll
+	LD	(BkpRFlg),A
+	LD	A,D
+	OR	E
+	JR	Z,BkpCF_SuccessCloseAll
+	PUSH	DE
+	LD	HL,FuncBuffer
+	POP	DE
+	LD	A,(BkpDstH)
+	LD	C,#14			; DOS: Write
+	RST	#10
+	JR	C,BkpCF_FailCloseAll
+	LD	A,(BkpRFlg)
+	OR	A
+	JR	Z,BkpCF_Loop
+	JR	BkpCF_SuccessCloseAll
+
+BkpCF_FailCloseAll:
+	CALL	BkpCF_DoCloseAll
+	SCF
+	RET
+
+BkpCF_SuccessCloseAll:
+	CALL	BkpCF_DoCloseAll
+	OR	A
+	RET
+
+BkpCF_FailCloseSrc:
+	CALL	BkpCF_DoCloseSrc
+	SCF
+	RET
+
+BkpCF_Fail:
+	SCF
+	RET
+
+BkpCF_DoCloseAll:
+	LD	A,(BkpDstH)
+	CP	#FF
+	JR	Z,BkpCF_DoCloseSrc
+	LD	C,#12			; DOS: Close
+	RST	#10
+BkpCF_DoCloseSrc:
+	LD	A,(BkpSrcH)
+	CP	#FF
+	RET	Z
+	LD	C,#12			; DOS: Close
+	RST	#10
+	RET
+; Build BackupName from FileName.
+; Backup naming rule:
+; ext len 1    -> append '__' (test.c -> test.c__)
+; ext len 2    -> append '_'  (test.cc -> test.cc_)
+; ext len >=3  -> replace last ext char (test.asm -> test.as_)
+; If no extension, replace last character of basename.
+; Carry set on failure.
+BuildBackupName:
+	LD	HL,BkpSrcName
+	LD	DE,BackupName
+	LD	B,#7F
+BkpNmCp:
+	LD	A,(HL)
+	RES	7,A
+	LD	(DE),A
+	INC	HL
+	INC	DE
+	OR	A
+	JR	Z,BkpNmEnd
+	DJNZ	BkpNmCp
+	XOR	A
+	LD	(DE),A
+BkpNmEnd:
+	LD	HL,BackupName
+	LD	(BkpBasePtr),HL
+	LD	HL,#0000
+	LD	(BkpDotPtr),HL
+	LD	HL,#0000
+	LD	(BkpLastPtr),HL
+	LD	HL,BackupName
+BkpNmScan:
+	LD	A,(HL)
+	OR	A
+	JR	Z,BkpNmDecide
+	LD	(BkpLastPtr),HL
+	CP	#5C
+	JR	Z,BkpNmSep
+	CP	'/'
+	JR	Z,BkpNmSep
+	CP	':'
+	JR	Z,BkpNmSep
+	CP	'.'
+	JR	NZ,BkpNmNext
+	LD	(BkpDotPtr),HL
+	JR	BkpNmNext
+BkpNmSep:
+	INC	HL
+	LD	(BkpBasePtr),HL
+	LD	DE,#0000
+	LD	(BkpDotPtr),DE
+	DEC	HL
+BkpNmNext:
+	INC	HL
+	JR	BkpNmScan
+
+BkpNmDecide:
+	LD	HL,(BkpDotPtr)
+	LD	A,H
+	OR	L
+	JR	Z,BkpNmNoExt
+	INC	HL
+	LD	A,(HL)
+	OR	A
+	JR	Z,BkpNmNoExt
+	LD	C,#00
+BkpExtSeek:
+	LD	A,(HL)
+	OR	A
+	JR	Z,BkpExtClass
+	INC	C
+	INC	HL
+	JR	BkpExtSeek
+
+BkpExtClass:
+	LD	A,C
+	CP	#01
+	JR	Z,BkpExtAppend2
+	CP	#02
+	JR	Z,BkpExtAppend1
+	CP	#03
+	JR	NC,BkpExtReplaceLast
+	JR	BkpNmNoExt
+
+BkpExtAppend2:
+	LD	A,'_'
+	LD	(HL),A
+	INC	HL
+	LD	A,'_'
+	LD	(HL),A
+	INC	HL
+	XOR	A
+	LD	(HL),A
+	OR	A
+	RET
+
+BkpExtAppend1:
+	LD	A,'_'
+	LD	(HL),A
+	INC	HL
+	XOR	A
+	LD	(HL),A
+	OR	A
+	RET
+
+BkpExtReplaceLast:
+	DEC	HL
+	LD	A,'_'
+	LD	(HL),A
+	OR	A
+	RET
+
+BkpNmNoExt:
+	LD	HL,(BkpLastPtr)
+	LD	A,H
+	OR	L
+	JR	Z,BkpNmFail
+	LD	DE,(BkpBasePtr)
+	OR	A
+	SBC	HL,DE
+	JR	C,BkpNmFail
+	LD	HL,(BkpLastPtr)
+	LD	A,'_'
+	LD	(HL),A
+	OR	A
+	RET
+
+BkpNmFail:
+	SCF
+	RET
+
+; Build normalized source path in BkpSrcName.
+; Strips trailing :0/:1 window suffix if present.
+BuildSourceName:
+	LD	HL,FileName
+	LD	DE,BkpSrcName
+	LD	B,#7F
+BSrcCp:
+	LD	A,(HL)
+	BIT	7,A
+	JR	Z,BSrcNoHi
+	RES	7,A
+	LD	(DE),A
+	INC	DE
+	XOR	A
+	LD	(DE),A
+	JR	BSrcEnd
+
+BSrcNoHi:
+	CP	#21
+	JR	C,BSrcTerm
+	LD	(DE),A
+	INC	HL
+	INC	DE
+	DJNZ	BSrcCp
+
+BSrcTerm:
+	XOR	A
+	LD	(DE),A
+	JR	BSrcEnd
+BSrcEnd:
+	LD	HL,BkpSrcName
+	LD	DE,#0000
+BSrcSeek:
+	LD	A,(HL)
+	OR	A
+	JR	Z,BSrcChk
+	LD	D,H
+	LD	E,L
+	INC	HL
+	JR	BSrcSeek
+BSrcChk:
+	LD	A,D
+	OR	E
+	RET	Z
+	LD	A,(DE)
+	CP	'0'
+	JR	Z,BSrcPrev
+	CP	'1'
+	RET	NZ
+BSrcPrev:
+	DEC	DE
+	LD	A,(DE)
+	CP	':'
+	RET	NZ
+	XOR	A
+	LD	(DE),A
+	RET
 ;[]===========================================================[]
 
 SaveFileAs
@@ -532,6 +840,22 @@ FileHandle
 	BYTE	#00			; File handle
 FileName
 	BLOCK	#80,0		; And name
+BkpSrcH
+	BYTE	#00
+BkpDstH
+	BYTE	#00
+BkpRFlg
+	BYTE	#00
+BackupName
+	BLOCK	#80,0
+BkpSrcName
+	BLOCK	#80,0
+BkpBasePtr
+	DEFW	#0000
+BkpDotPtr
+	DEFW	#0000
+BkpLastPtr
+	DEFW	#0000
 ;[]===========================================================[]
 ; Window for
 SureWin	LD	IX,TxtWtab
